323338bbaae6bb4680ea6b5512ef31a33594c1d5
Tyler <tyler@magebinary.com>
2021-06-28 15:19:23 +0700

    Code refactor

--- templates/images_api/snippet.php
+++ templates/images_api/snippet.php
@@ -4,13 +4,13 @@
  */
 ?>
 <?php
-    $paymentTerm = $this->getPaymentTerm();
-    $paymentTerm = ($paymentTerm && is_array($paymentTerm) && !empty($paymentTerm)) ?
-        '&term=' . implode(',', $paymentTerm) :
-        '';
-    $snippetUrl = $this->getImagesApiUrl() . $this->getSnippetPath();
-    $fullBlock = $this->isFullBlock() ? '&full_block=1' : '';
+	$paymentTerm = $this->getPaymentTerm();
+	$paymentTerm = ( $paymentTerm && is_array( $paymentTerm ) && ! empty( $paymentTerm ) ) ?
+		'&term=' . implode( ',', $paymentTerm ) :
+		'';
+	$snippetUrl  = $this->getImagesApiUrl() . $this->getSnippetPath();
+	$fullBlock   = $this->isFullBlock() ? '&full_block=1' : '';
 ?>
-<img class="lpay_snippet" src="<?php echo $snippetUrl ?>?amount=<?php echo $this->getAmount(); ?><?php echo $fullBlock; ?><?php echo $paymentTerm; ?>"
-     alt="<?php echo $this->getTitle(); ?>"/>
+<img class="lpay_snippet" src="<?php echo $snippetUrl; ?>?amount=<?php echo $this->getAmount(); ?><?php echo $fullBlock; ?><?php echo $paymentTerm; ?>"
+	 alt="<?php echo $this->getTitle(); ?>"/>
 
